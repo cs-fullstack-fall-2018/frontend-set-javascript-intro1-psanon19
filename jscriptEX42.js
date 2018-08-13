0133e40eaa48b86c22bef18e8c3472381f367e6f
Pelane Sanon <p_sanon@aol.com>
2018-08-13 10:50:30 -0500

    Check In

---
+++ jscriptEX42.js
@@ -0,0 +1,9 @@
+document.getElementById("myNewChange").innerText="Where i input a text change";
+
+document.getElementById("secondChange").innerText="This is my site now";
+
+var myName=document.getElementById("thirdChange").innerText;
+
+document.getElementById("thirdChange").innerText=myName+" Pj";
+
+document.getElementById("kennButton").innerText="THIS IS MY BUTTON";
